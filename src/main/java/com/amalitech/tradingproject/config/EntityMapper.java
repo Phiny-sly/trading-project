b7bb7d041716548599bea178410015c19f87314a
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-01 13:01:36 +0000

    entity mapper created

--- src/main/java/com/amalitech/tradingproject/config/EntityMapper.java
+++ src/main/java/com/amalitech/tradingproject/config/EntityMapper.java
@@ -1,2 +1,30 @@
-package com.amalitech.tradingproject.config;public interface EntityMapper {
+package com.amalitech.tradingproject.config;
+
+import com.amalitech.tradingproject.dto.UserDto;
+import com.amalitech.tradingproject.entity.User;
+import com.amalitech.tradingproject.payload.UserPayload;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
+
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Component
+public interface EntityMapper {
+    EntityMapper INSTANCE = Mappers.getMapper(EntityMapper.class);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "role", target = "role")
+    UserDto convertToUserDto(User user);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "role", target = "role")
+    @Mapping(source = "password",target = "password")
+    User convertToUser(UserPayload userPayload);
+
+    void updateUserDetails(@MappingTarget User user, UserPayload userPayload);
 }
