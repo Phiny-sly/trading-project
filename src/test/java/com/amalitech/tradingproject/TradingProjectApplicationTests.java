51577ca98e67fe76180b3d5df8799e366be2bbce
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-01 09:14:38 +0000

    initial commit

---
+++ src/test/java/com/amalitech/tradingproject/TradingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.tradingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
