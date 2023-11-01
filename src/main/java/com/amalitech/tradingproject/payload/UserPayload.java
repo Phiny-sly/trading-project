b6c3d53dffddc6f010068f9598ef5901d7530adc
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-01 13:05:41 +0000

    user payload created

--- src/main/java/com/amalitech/tradingproject/payload/UserPayload.java
+++ src/main/java/com/amalitech/tradingproject/payload/UserPayload.java
@@ -1,10 +1,12 @@
 package com.amalitech.tradingproject.payload;
 
+import com.amalitech.tradingproject.entity.Role;
 import lombok.Data;
 
 @Data
-public class CreateUserPayload {
+public class UserPayload {
     private String name;
     private String email;
     private String password;
+    private Role role;
 }
