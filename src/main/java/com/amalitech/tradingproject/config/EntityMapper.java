7dd30162241b2cc07fcad0733ba68fe01c6f02b5
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-02 00:48:23 +0000

    entity conversion for order and product added

--- src/main/java/com/amalitech/tradingproject/config/EntityMapper.java
+++ src/main/java/com/amalitech/tradingproject/config/EntityMapper.java
@@ -1,7 +1,13 @@
 package com.amalitech.tradingproject.config;
 
+import com.amalitech.tradingproject.dto.OrderDto;
+import com.amalitech.tradingproject.dto.ProductDto;
 import com.amalitech.tradingproject.dto.UserDto;
+import com.amalitech.tradingproject.entity.Order;
+import com.amalitech.tradingproject.entity.Product;
 import com.amalitech.tradingproject.entity.User;
+import com.amalitech.tradingproject.payload.OrderPayload;
+import com.amalitech.tradingproject.payload.ProductPayload;
 import com.amalitech.tradingproject.payload.UserPayload;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -18,13 +24,32 @@ public interface EntityMapper {
     @Mapping(source = "name", target = "name")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "role", target = "role")
+    @Mapping(source = "id", target = "id")
     UserDto convertToUserDto(User user);
 
     @Mapping(source = "name", target = "name")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "role", target = "role")
-    @Mapping(source = "password",target = "password")
+    @Mapping(source = "password", target = "password")
     User convertToUser(UserPayload userPayload);
 
     void updateUserDetails(@MappingTarget User user, UserPayload userPayload);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userId", expression = "java(order.getUser().getId())")
+    @Mapping(target = "listOfProductLines", source = "listOfProductLines")
+    OrderDto convertToOrderDto(Order order);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "stock", source = "stock")
+    ProductDto convertToProductDto(Product product);
+
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "stock", source = "stock")
+    Product convertToProduct(ProductPayload productPayload);
+
+    void updateProductDetails(@MappingTarget Product product, ProductPayload productPayload);
 }
