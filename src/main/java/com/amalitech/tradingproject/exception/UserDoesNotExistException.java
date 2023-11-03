3e20269c2fff678168a4dd5393165e603c7888f2
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-03 19:08:16 +0000

    error message for email added

--- src/main/java/com/amalitech/tradingproject/exception/UserDoesNotExistException.java
+++ src/main/java/com/amalitech/tradingproject/exception/UserDoesNotExistException.java
@@ -4,4 +4,8 @@ public class UserDoesNotExistException extends RuntimeException {
     public UserDoesNotExistException(long id) {
         super(String.format("User with id %d does not exist", id));
     }
+
+    public UserDoesNotExistException(String email) {
+        super(String.format("User with email %s does not exist", email));
+    }
 }
