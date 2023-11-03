410cc2b8aa489dc48aded038c6945a2758d61f5e
Phiny-sly <phinehasanteh2@gmail.com>
2023-11-03 19:04:49 +0000

    bcrypt encryptor created

--- src/main/java/com/amalitech/tradingproject/config/AppConfig.java
+++ src/main/java/com/amalitech/tradingproject/config/AppConfig.java
@@ -1,20 +1,14 @@
 package com.amalitech.tradingproject.config;
 
-import com.amalitech.tradingproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
